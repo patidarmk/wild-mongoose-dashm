9853dda97e1503d3ef69d4aee88b22bea54bba09
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-09 13:02:44 +0530

    Add src/pages/Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { TodoApp } from "@/components/todo/TodoApp";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <TodoApp />
+    </div>
+  );
+};
+
+export default Index;
